06924d0ddf6bc0d1d67861b84b7c078bb9a936df
João Scaleão <j.scaleao@hotmail.com>
2021-02-18 02:17:47 -0300

    refactor front home with laravel-mix

--- acauem/resources/views/templetes/home.blade.php
+++ acauem/resources/views/templetes/home.blade.php
@@ -7,15 +7,18 @@
         <meta name="author" content="">
         <title>@yield('titulo')</title>
         <!-- Bootstrap core CSS -->
-        <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <!--<link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">-->
+        <link href="{{ asset('homeCss/bootstrap.css') }}" rel="stylesheet">
 
         <!-- Custom fonts for this template -->
-        <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+        <!--<link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">-->
+        <link href="{{ asset('homeCss/fontawesome.css') }}" rel="stylesheet">
         <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
         <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 
         <!-- Custom styles for this template -->
-        <link href="../css/clean-blog.min.css" rel="stylesheet">
+        <!--<link href="../css/clean-blog.min.css" rel="stylesheet">-->
+        <link href="{{ asset('homeCss/clean-blog.css') }}" rel="stylesheet">
     </head>
     <body>
         <!-- HEADER ----------------------------->
@@ -44,9 +47,14 @@
         <!-- FOOTER ----------------------------->
 
         <!-- Bootstrap core JavaScript -->
-        <script src="../vendor/jquery/jquery.min.js"></script>
-        <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <!--<script src="../vendor/jquery/jquery.min.js"></script>
+        <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script> -->
+        <scrip src="{{asset('homeJs/jquery.js')}}"></script>
+        <scrip src="{{asset('homeJs/bootstrap.bundle.js')}}"></script>
+
         <!-- Custom scripts for this template -->
-        <script src="../js/clean-blog.min.js"></script>
+        <!-- <script src="../js/clean-blog.min.js"></script> -->
+        <scrip src="{{asset('homeJs/clean-blog.js')}}"></script>
+
     </body>
 </html>
--- acauem/webpack.mix.js
+++ acauem/webpack.mix.js
@@ -11,7 +11,11 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix
+
+    .styles('resources/views/templetes/homeCss/bootstrap/bootstrap.css', 'public/homeCss/bootstrap.css')
+    .styles('resources/views/templetes/homeCss/fontawesome-free/css/all.css', 'public/homeCss/fontawesome.css')
+    .styles('resources/views/templetes/homeCss/clean-blog/clean-blog.css', 'public/homeCss/clean-blog.css') 
+    .scripts('resources/views/templetes/homeJs/clean-blog/clean-blog.js', 'public/homeJs/clean-blog.js')
+    .scripts('resources/views/templetes/homeJs/bootstrap/bootstrap.bundle.js', 'public/homeJs/bootstrap.bundle.js')
+    .scripts('node_modules/jquery/dist/jquery.js', 'public/homeJs/jquery.js');
