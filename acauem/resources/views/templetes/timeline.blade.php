250f1c4f37bb47070d56df88caadb419282b93d1
João Scaleão <j.scaleao@hotmail.com>
2021-04-25 18:23:47 -0300

    Adjustment asset css timeline with laravel-mix/web-pack

--- acauem/resources/views/templetes/timeline.blade.php
+++ acauem/resources/views/templetes/timeline.blade.php
@@ -8,7 +8,7 @@
         <!-- original <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous"> -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <!-- Our Custom CSS -->
-        <link rel="stylesheet" href="../css/styleTimeline.css">
+        <link rel="stylesheet" href="{{ asset('timelineCss/styleTimeline.css') }}">
         <!-- Font Awesome JS -->
         <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
         <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
--- acauem/webpack.mix.js
+++ acauem/webpack.mix.js
@@ -15,7 +15,10 @@ mix
 
     .styles('resources/views/templetes/homeCss/bootstrap/bootstrap.css', 'public/homeCss/bootstrap.css')
     .styles('resources/views/templetes/homeCss/fontawesome-free/css/all.css', 'public/homeCss/fontawesome.css')
-    .styles('resources/views/templetes/homeCss/clean-blog/clean-blog.css', 'public/homeCss/clean-blog.css') 
+    .styles('resources/views/templetes/homeCss/clean-blog/clean-blog.css', 'public/homeCss/clean-blog.css')
+
+    .styles('resources/views/templetes/timelineCss/styleTimeline.css', 'public/timelineCss/styleTimeline.css')
+
     .scripts('resources/views/templetes/homeJs/clean-blog/clean-blog.js', 'public/homeJs/clean-blog.js')
     .scripts('resources/views/templetes/homeJs/bootstrap/bootstrap.bundle.js', 'public/homeJs/bootstrap.bundle.js')
     .scripts('node_modules/jquery/dist/jquery.js', 'public/homeJs/jquery.js');
